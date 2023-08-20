8b1c427d60dc9256fbcebea7fa5c40c7f1c1c3b2
zidan-p <zidanputrarahman153@gmail.com>
2023-08-20 23:22:42 +0700

    error next js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  webpack : (config, options) => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    console.log("run webpack config in next . config");
+    
+    config.module.rules.unshift(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      // {
+      //   ...fileLoaderRule,
+      //   test: /\.svg$/i,
+      //   resourceQuery: /url/, // *.svg?url
+      // },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
+
+    console.log("edited rules");
+    console.log(config.module.rules);
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
+}
--- src/feature/darkMode/ui/ToggleDarkMode/index.tsx
+++ src/feature/darkMode/ui/ToggleDarkMode/index.tsx
@@ -4,7 +4,6 @@ import Moon from "./../icons/moon.svg";
 
 
 
-""
 export function ToggleDarkMode(){
   return(
     <div className="flex w-10 rounded-full bg-white">
