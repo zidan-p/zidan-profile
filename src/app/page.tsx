91d5fde1b26b5c3c846ad934464c353992296ded
zidan-p <zidanputrarahman153@gmail.com>
2023-08-21 23:56:41 +0700

    add main page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,49 @@
 import Image from 'next/image'
+import profileImage from "./asset/img/profile.png"
+import GithubIcon from "./asset/icons/github.svg";
+import FileTextIcon from "./asset/icons/fileText.svg";
+import LinkedInIcon from "./asset/icons/linkedin.svg";
 
 export default function Home() {
   return (
     <main>
-      Heloo all, what uppp 👋👋👋👋👋
-      <p className='mb-3 dark:text-gray-300'>
-      Hi, I am Zidan, A fresh graduate student from vocational high school from SMKS Telkom Sandhy Putra Malang. I Have some knowledges and experiences building web app from my school. 
-
-      At first, When I was just starting my education in high school (maybe june 2020?), I didn’t know anything about programing beside how to write website with html. starting from that time, I gradually learn how to code with general instruction structure like looping, function, variable etc. Although the language i wrote my code is different, the general logic still the same.  
-
-      Now I already knew and have experience with web development, whether it’s in frontend or backend. Currently I want to learn java in game development while expanding my knowledge in web development.
-      </p>
-      <p className='mb-3 dark:text-gray-300'>
-      Hi, I am Zidan, A fresh graduate student from vocational high school from SMKS Telkom Sandhy Putra Malang. I Have some knowledges and experiences building web app from my school. 
-
-      At first, When I was just starting my education in high school (maybe june 2020?), I didn’t know anything about programing beside how to write website with html. starting from that time, I gradually learn how to code with general instruction structure like looping, function, variable etc. Although the language i wrote my code is different, the general logic still the same.  
-
-      Now I already knew and have experience with web development, whether it’s in frontend or backend. Currently I want to learn java in game development while expanding my knowledge in web development.
-      </p>
-      <p className='mb-3 dark:text-gray-300'>
-      Hi, I am Zidan, A fresh graduate student from vocational high school from SMKS Telkom Sandhy Putra Malang. I Have some knowledges and experiences building web app from my school. 
-
-      At first, When I was just starting my education in high school (maybe june 2020?), I didn’t know anything about programing beside how to write website with html. starting from that time, I gradually learn how to code with general instruction structure like looping, function, variable etc. Although the language i wrote my code is different, the general logic still the same.  
-
-      Now I already knew and have experience with web development, whether it’s in frontend or backend. Currently I want to learn java in game development while expanding my knowledge in web development.
-      </p>
-      <p className='mb-3 dark:text-gray-300'>
-      Hi, I am Zidan, A fresh graduate student from vocational high school from SMKS Telkom Sandhy Putra Malang. I Have some knowledges and experiences building web app from my school. 
-
-      At first, When I was just starting my education in high school (maybe june 2020?), I didn’t know anything about programing beside how to write website with html. starting from that time, I gradually learn how to code with general instruction structure like looping, function, variable etc. Although the language i wrote my code is different, the general logic still the same.  
-
-      Now I already knew and have experience with web development, whether it’s in frontend or backend. Currently I want to learn java in game development while expanding my knowledge in web development.
-      </p>
+      <section className="flex justify-between">
+        <div className="">
+          <p>Hello there 👋, it's me</p>
+          <h1 className='text-5xl font-bold text-gray-500'>
+            <span className='text-yellow-400'>Zidan </span>
+            Putra Rahman
+          </h1>
+          <p>A Web developer from East Java - Indonesia</p>
+          <div className="flex gap-8 mt-5">
+            <a href='https://github.com/zidan-p' className="inline-flex gap-1 items-center hover:underline">
+              <GithubIcon />
+              <span>Github</span>
+            </a>
+            <a href='#' className="inline-flex gap-1 items-center hover:underline">
+              <FileTextIcon />
+              <span>My Resume</span>
+            </a>
+            <a href='https://www.linkedin.com/in/zidan-putra-rahman-546b6b232/' className="inline-flex gap-1 items-center hover:underline">
+              <LinkedInIcon />
+              <span>LinkedIn</span>
+            </a>
+          </div>
+        </div>
+        <div className="">
+          <Image src={profileImage} alt='hello' placeholder='blur' width={170} height={158.8} />
+        </div>
+      </section>
+        <hr className='mt-6' />
+      <section className='mt-6'>
+        <p className=''>
+          Fresh graduate from vocational school in software engineering major. I have Knowledge and Experience building Web Application, especially in Frontend Development.
+        </p>
+      </section>
+      <section className='mt-4'>
+        <h3 className='text-yellow-400 font-semibold'>Experiences</h3>
+        <p className='text-sm'>Wheter it's from internship, work or another activities</p>
+      </section>
     </main>
   )
 }
